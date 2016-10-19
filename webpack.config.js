6b5b3de96aad49f879657c3f1afb45656848906d
Olli Karppinen <olli.karppinen@relex.fi>
2016-10-19 23:05:30 +0300

    Adds npm dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,20 @@ var webpack = require('webpack')
 var BundleTracker = require('webpack-bundle-tracker')
 
 module.exports = {
-    //the base directory (absolute path) for resolving the entry option
     context: __dirname,
-    //the entry point we created earlier. Note that './' means
-    //your current directory. You don't have to specify the extension  now,
-    //because you will specify extensions later in the `resolve` section
     entry: './assets/js/index',
 
     output: {
-        //where you want your compiled bundle to be stored
         path: path.resolve('./assets/bundles/'),
-        //naming convention webpack should use for your files
-        filename: '[name]-[hash].js',
+        filename: '[name].js',
     },
 
     plugins: [
-        //tells webpack where to store data about your bundles.
         new BundleTracker({filename: './webpack-stats.json'}),
     ],
 
     module: {
         loaders: [
-            //a regexp that tells webpack use the following loaders on all
-            //.js and .jsx files
             {
               test: /\.jsx?$/,
               exclude: /node_modules/,
@@ -39,9 +30,7 @@ module.exports = {
     },
 
     resolve: {
-        //tells webpack where to look for modules
         modulesDirectories: ['node_modules'],
-        //extensions that should be used to resolve modules
         extensions: ['', '.js', '.jsx']
     }
 }
